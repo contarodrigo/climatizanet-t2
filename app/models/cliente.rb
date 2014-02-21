10b7b5227301adbe9074944d7ef046cdc8ff0d6f
Rodrigo <contarodrigo@gmail.com>
2014-02-21 20:32:16 -0300

    primeiras alteracoes model

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,5 @@
 class Cliente < ActiveRecord::Base
+
+	validates_presence_of :nome, :email, :telefone
+
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,17 @@
 class Usuario < ActiveRecord::Base
+	EMAIL_REGEXP=/\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+	validates_presence_of :nome, :email, :senha
+
+	validates_confirmation_of :senha
+
+	validates_uniqueness_of :email
+
+	validate :email_format
+	private
+	def email_format
+		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+	end
+
+
 end
