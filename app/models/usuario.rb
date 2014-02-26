76e253a1a0679189ac2664565be6db397fdc4c9f
Rodrigo <contarodrigo@gmail.com>
2014-02-25 23:14:19 -0300

    Ajustes usuario conforme Colchonet

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,17 +1,22 @@
 class Usuario < ActiveRecord::Base
 	EMAIL_REGEXP=/\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
-	validates_presence_of :nome, :email, :senha
+	validates_presence_of :nome, :email
 
-	validates_confirmation_of :senha
+	#validates_confirmation_of :senha,
+	# o has_secure_password ja faz as valadoes porem temos que mudar o nome do atributo para password
+	has_secure_password
+
 
 	validates_uniqueness_of :email
 
 	validate :email_format
+
+
 	private
+
+	#validacao para o formato do email conforme expressao regular EMAIL_REGEXP
 	def email_format
 		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
 	end
-
-
 end
