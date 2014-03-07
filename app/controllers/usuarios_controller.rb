6ce23e65a0ebda113245e4fd08accd4940f80032
Rodrigo <contarodrigo@gmail.com>
2014-03-06 21:45:40 -0300

    correção confirmação de senha login

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -48,6 +48,6 @@ class UsuariosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def usuario_params
-      params.require(:usuario).permit(:nome, :email, :password)
+      params.require(:usuario).permit(:nome, :email, :password, :password_confirmation)
     end
 end
