be514de7087ecc610bdf5026aaba15f545dcc376
emaffezzoli <edenilsonmaffezzoli@gmail.com>
2014-03-29 11:03:19 -0300

    Mais ajustes Item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -80,6 +80,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:qtdequipamento, :qtdtubulacao, :qtdextra, :tubulacao, :extra, :valor, :descricao)
+      params.require(:item).permit(:qtdequipamento, :qtdtubulacao, :qtdextra, :tubulacao, :extra, :valor, :descricao, :tipo_Servico, :tipo_produto, :produto, :potencia, :unidade_medida, :info)
     end
 end
